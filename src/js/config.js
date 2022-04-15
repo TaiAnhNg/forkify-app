73c21a2c1cf7b510ff32938a7cb8e3a7bee1fb25
TaiAnhNg <nguyentaianh41099@gmail.com>
2022-04-15 18:25:34 +1000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULT_PER_PAGE = 10;
+export const KEY = 'c5dc9461-f161-4bc1-8861-f308cd67ccee';
+export const MODAL_CLOSE_SEC = 2;
